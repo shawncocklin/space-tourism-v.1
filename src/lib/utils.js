7c4e524e6ab781486962f8e58573d18bd7375c16
shawncocklin <shawncocklin@gmail.com>
2022-04-29 16:48:13 -0400

    clened up bg image function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,8 @@
 import {onMount} from 'svelte'
 
-// TODO: test if removing reference to current will break the logic
 
 export function setBackground(backgroundPath) {
   onMount(()=> {
-    let current = document.body.style['background-image']
-    console.log(current)
     document.body.style['background-image'] = backgroundPath
-    return () => document.body.style['background-image'] = current
   })
 }
-
-// module.exports = {setBackground}
